9b6736a330dc9c7e62d32cf3793ec4064de2a3e5
makhokit <makhorig@gmail.com>
2018-11-25 17:53:57 +0400

    Clean

--- tools/start_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-file=$GOPATH/src/github.com/freitx-project/freitx-network-blockchain/sampleconfig/stonevan/config_$1.yaml
-echo "Starting node with config file:" $file
-$GOPATH/src/github.com/freitx-project/freitx-network-blockchain/bin/server -stderrthreshold=WARNING -log_dir=./log -config=$file
-
-exit 0
